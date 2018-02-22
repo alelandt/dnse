2de68e4c9a91ddbedf571b22d85b58b5b93cc2ee
msteven <matthew@cs.odu.edu>
2018-02-22 14:08:18 -0500

    maps

--- dnse/gmaps.py
+++ dnse/gmaps.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse, HttpRequest
-
+import json
 import googlemaps
 
-def google_lookup(locationData):
-    temp = []
-    address = maps.geolocate()
-    results = google_maps.search(location=address)
+google_maps = googlemaps.Client(key='AIzaSyBs072jMQc_jZQF8ePeyVrhGk0HGyKFMXA')
+
+def google_lookup(lon, lat):
+    temp = {}
+    results = google_maps.reverse_geocode((lat,lon))
+    print(results)
     for entries in results:
-        if entries.shortname.contains(' ') == False:
-            temp.append(entries.short_name)
+        print(entries)
+        #if entries.shortname.contains(' ') == False:
+        #    temp.append(entries.short_name)
     return temp
