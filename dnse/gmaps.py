ae5bd885bf7f6d412e5be59b1184c04d9fd664e7
msteven <matthew@cs.odu.edu>
2018-02-22 16:16:03 -0500

    added numpy unique and return array to gmaps

--- dnse/gmaps.py
+++ dnse/gmaps.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponse, HttpRequest
 import json
 import googlemaps
+import numpy as np
 
 google_maps = googlemaps.Client(key='AIzaSyBs072jMQc_jZQF8ePeyVrhGk0HGyKFMXA')
 
 def google_lookup(lon, lat):
     temp = []
     results = google_maps.reverse_geocode((lat,lon))
-    print(results)
+    #print(results)
     for entries in results:
         for items in entries.get('address_components'):
             if str.isdigit(items.get('long_name')) != True and items.get('long_name') != 'United States':
                 temp.append(items.get('short_name'))
             if str.isdigit(items.get('short_name')) != True and items.get('short_name') != 'United States':
                 temp.append(items.get('short_name'))
-    print(temp)
-#return temp
+    #print(np.unique(temp))
+return np.unique(temp)
