fa8a87925a9ad6fdba2464af3f3b7a0490aa6dce
ALeleT <atuck007@odu.edu>
2018-02-22 11:44:53 -0500

    local

--- dnse/views.py
+++ dnse/views.py
@@ -3,6 +3,9 @@ from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import forms
+import googlemaps
+
+google_maps = googlemaps.Client(key='AIzaSyBtrgoXtMw1l4TBaRfUFteqps4EoKGLBc')
 
 def index(request):
     form = forms.SearchForm()
@@ -11,3 +14,12 @@ def index(request):
 @csrf_exempt
 def search_results(request):
     return JsonResponse(request.POST.get('search_q'), safe=False)
+
+def google_lookup(request):
+    temp = []
+    address = maps.geolocate()
+    results = google_maps.search(location=address)
+    for entries in results:
+        if entries.shortname.contains(' ') == False:
+            temp.append(entries.short_name)
+    return temp
