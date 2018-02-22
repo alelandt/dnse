a1f8234c469684e31eec5090911e8985a8d64d3b
ALeleT <atuck007@odu.edu>
2018-02-22 10:59:08 -0500

    working ajax

--- dnse/views.py
+++ dnse/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from . import forms
 
 def index(request):
-    return render(request, "index.html")
+    form = forms.SearchForm()
+    return render(request, "index.html", {'form': form})
 
+@csrf_exempt
 def search_results(request):
-    return JsonResponse("empty")
+    return JsonResponse("empty", safe=False)
