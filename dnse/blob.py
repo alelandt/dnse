d43c4c278b8611b77214aba2c50c6259b4d38822
ALeleT <atuck007@odu.edu>
2018-02-23 12:49:27 -0500

    test

--- dnse/blob.py
+++ dnse/blob.py
@@ -39,14 +39,11 @@ def strip_out(name):
     cleared = str.replace(cleared, ",", "")
     return cleared
 
-def combine_all(locations, syn, tldd, addtld):
+def combine_all(locations, syn, tldd):
     garbage = list(map(''.join, itertools.chain(itertools.product(locations, syn), itertools.product(syn, locations))))
     garbage += list(map(''.join, itertools.chain(itertools.product(syn, syn))))
     garbage += syn;
-    if addtld == True:
-        topGarbage = list(map('.'.join, itertools.chain(itertools.product(garbage,tldd))))
-    else:
-        topGarbage = garbage
+    topGarbage = list(map('.'.join, itertools.chain(itertools.product(garbage,tldd))))
 
     return topGarbage
     
