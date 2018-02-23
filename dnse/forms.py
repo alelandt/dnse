de86b349a8cccd1026856cd3174248633efe19bf
ALeleT <atuck007@odu.edu>
2018-02-22 20:19:59 -0500

    templates

--- dnse/forms.py
+++ dnse/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search_query = forms.CharField(label="", max_length=128)
+    search_query = forms.CharField(label="", max_length=128, widget=forms.TextInput(attrs={'class': 'input-form'}),)
