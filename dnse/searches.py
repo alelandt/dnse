fcb96b3a5546621cfbbb47fcb973ca210f5d02f3
prairiedogstyle20 <lwm209@email.vccs.edu>
2018-02-22 22:13:39 -0500

    adding attempt at verisign name search

--- dnse/searches.py
+++ dnse/searches.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 import requests
 import numpy as np
+import json
 
 def name_parse(names):
     temp =[]
@@ -16,6 +17,23 @@ def name_parse(names):
    # 676226de70489ae087ba1cd63cf9345a # api key
     return np.unique(temp) 
 
-def verisign_lookup(names):
-    return "junk"
+def verisign_mass_lookup(names):
+   temp=[]
+   queryName = names
+   queSt =  ','.join(queryName)
+        url='https://sugapi.verisign-grs.com/ns-api/2.0/bulk-check'#?names=' + queSt + '=house,car,boat,com,org,xxx'  
+        json.dumps(queryName)
+    header={'X-NAMESUGGESTION:676226de70489ae087ba1cd63cf9345a'}
+    
+    r=requests.get(url, headers=headers)
+   results=r.text
+   for entries in results:
+        for items in entries.get('results'):
+                temp.append("name")
+   # 676226de70489ae087ba1cd63cf9345a # api key
+    return np.unique(temp)
+
+
+     return "junk"
+
 
